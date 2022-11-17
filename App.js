5753139181ebac02960d54468aae35d87560ce7f
Onkar Kanse <OnkarKanse18@hmail.com>
2022-11-17 11:38:03 +0530

    Done

--- App.js
+++ App.js
@@ -1,32 +1,42 @@
 import React from "react";
-import {View,StyleSheet,Button,Alert} from "react-native";
-//import Button from "./src/screens/Button";
+import { View, StyleSheet, Button,Text, Alert, TouchableOpacity } from "react-native";
 
 
-const App =()=>{
-  return(
+
+const App = () => {
+  return (
     <View style={Styles.v}>
-            <Button title=""
-            onPress={()=>{
-               // console.log("Pressed ");
-            Alert.alert("Hello There!!")
-            }}/>
-    </View>
+      <TouchableOpacity
+        onPress={() => {
+          // console.log("Pressed ");
+          Alert.alert("Y")
+        }}>
+        <Text>Pressed On </Text>
+      </TouchableOpacity>
+      <Button
+      disabled
+           title="Join ME"
+        onPress={() => {
+          // console.log("Pressed ");
+          Alert.alert("YS")
+        }}/>
+       
+          </View>
   )
 };
-const Styles= StyleSheet.create({
-  v:{
-    height:760,
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center"
+const Styles = StyleSheet.create({
+  v: {
+    height: 760,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   },
-  i:{
-    height:100,
-    width:100,
-    margin:12,
-    borderColor:'blue',
-    borderWidth:5
+  i: {
+    height: 100,
+    width: 100,
+    margin: 12,
+    borderColor: 'blue',
+    borderWidth: 5
   }
 });
 
